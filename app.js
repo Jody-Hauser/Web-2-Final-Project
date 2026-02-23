1530565c00feb784094348eabe1863884cb74afc
Jody Hauser <hasuerj3@students.westerntc.edu>
2026-02-23 14:16:24 -0600

    step 5 done

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 // allow the app to get data for form submits
 app.use(bodyParser.urlencoded({ extended: true }));
+// Redirect to HTTPS
+app.use((req, res, next) => {
+   if (process.env.NODE_ENV === 'production') {
+      if (req.headers['x-forwarded-proto'] !== 'https')
+         // the statement for performing our redirection
+         return res.redirect('https://' + req.headers.host + req.url);
+      else
+         return next();
+   }else{
+      return next();
+   }
+});
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -53,8 +65,38 @@ app.get('/contact', (req, res) => {
      title: "Contact Me"
   });
 });
-app.post('/contact/submit', (req, res) => {
-  res.send("<h1>TODO: Handle contact form posts</h1>" + JSON.stringify(req.body));
+aapp.post('/contact/submit', (req, res) => {
+
+  // import the helper functions that we need
+  const {isValidContactFormSubmit, sendEmailNotification} = require("./modules/contact-helpers");
+
+  // Destructure the req.body object into variables
+  const {firstName, lastName, email, comments} = req.body;
+
+  // Validate the variables
+  if(isValidContactFormSubmit(firstName, lastName, email, comments)){
+    // Everything is valid, so send an email to YOUR email address with the data entered into the form
+    const message = `From: ${firstName} ${lastName}\n
+                    Email: ${email}\n
+                    Message: ${comments}`;
+
+    sendEmailNotification(message, (err, info) => {
+  if(err){
+    console.log(err);
+    res.status(500).send("There was an error sending the email");
+  }else{
+    // Render a template that confirms the contact form info was recieved:
+    res.render("default-layout", {
+      title: "Contact Confirmation",
+      content: "<h2>Thank you for contacting me!</h2><p>I'll get back to you ASAP.</p>"
+    })
+  }
+});
+
+  }else{
+    res.status(400).send("Invalid request - data is not valid")
+  }
+
 });
 
 app.get('/', (req, res) => {
